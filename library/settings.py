3923e6c7bf4425ff0752296c52c83292075b7d4c
neunum <you@example.com>
2019-04-19 13:26:45 +0200

    Added files and changes required for deployment to heroku

--- library/settings.py
+++ library/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,3 +130,9 @@ EMAIL_HOST_PASSWORD = local.EMAIL_HOST_PASSWORD
 EMAIL_PORT = local.EMAIL_PORT
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
